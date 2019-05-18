0a7d9e2aa4548ecbc13b715f26ece0c771a110a5
David Harris <davidharris006@gmail.com>
2019-05-17 21:42:08 -0700

    customer bamazon completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,59 +3,99 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
-    
+
+
     port: 3306,
-  
+
 
     user: "root",
-  
-   
+
+
     password: "root",
     database: "bamazon"
-  });
+});
+
+var idShopping;
+var priceShopping;
+var quantityShopping;
 
 
 
+function getProducts() {
 
-function getProducts(){
-
-    connection.connect(function(err){
-        if (err){
+    connection.connect(function (err) {
+        if (err) {
             console.log(err);
         };
-        
+
         console.log('connected');
-        
+
         connection.query(
             'SELECT * FROM products;',
-            function(error,data){
+            function (error, data) {
                 console.log(data);
-                connection.end()
+
             }
         )
     })
 }
 
-getProducts();
+getProducts()
 
-inquirer
-  .prompt([
 
-    {
-      type: "input",
-      message: "What is the ID of the Item you would like to purchase?",
-      name: "productid"
-    },
-    
-    {
-      type: "input",
-      message: "How many would you like to buy?",
-      name: "quantity"
-    }
-  ])
-  .then(function(res) {
-    connection.query(
-        'SELECT * FROM products WHERE '
-    )
-  });
+
+function mainmenu() {
+
+
+    inquirer
+        .prompt([
+
+            {
+                type: "input",
+                message: "What is the ID of the Item you would like to purchase?",
+                name: "productid"
+            },
+
+            {
+                type: "input",
+                message: "How many would you like to buy?",
+                name: "quantity"
+            }
+        ])
+        .then(function (res) {
+
+            let productid = res.productid
+
+
+            connection.query(
+                `SELECT * FROM products WHERE Id LIKE ${productid}`,
+                function (error, data) {
+                    // console.log(data[0]);
+                    // console.log(data[0].stock_quantity + res.quantity);
+                    const currStock = parseFloat(data[0].stock_quantity)
+                    const amountPurchased = parseFloat(res.quantity)
+                    if (error) throw error;
+                    else if (currStock - amountPurchased <= 0) {
+
+                        mainmenu();
+                        console.log("There arent enough left!");
+                    }
+                    const newQuantity = currStock - amountPurchased;
+
+                    connection.query(
+                        `UPDATE products SET stock_quantity = ${newQuantity} WHERE Id LIKE ${productid}`,
+                        function(error){
+                            if (error) throw error
+
+                            getProducts()
+                            mainmenu()
+                        }
+                    )
+                }
+            )
+
+
+        });
+
+}
+mainmenu()
